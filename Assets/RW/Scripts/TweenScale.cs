854439d1e34f6624e909e6e4a7e599b2a775622b
Chase Bennett-Hill (1000106289) <BENNC9@student.op.ac.nz>
2023-02-24 14:36:49 +1300

    Started the Assignment task 01 started the script TweenScale

---
+++ Assets/RW/Scripts/TweenScale.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenScale : MonoBehaviour
+{
+    public float targetScale;
+    public float timeToReachTarget;
+    private float startScale;
+    private float percentScaled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
