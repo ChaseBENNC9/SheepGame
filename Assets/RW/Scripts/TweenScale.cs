1010c375a5b28c2c25e1ae04133b1dfb6b35924c
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-02-26 21:34:27 +1300

    Added an animated heart effect and a shrinking effect to the sheep when a sheep gets hit

--- Assets/RW/Scripts/TweenScale.cs
+++ Assets/RW/Scripts/TweenScale.cs
@@ -12,12 +12,17 @@ public class TweenScale : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        startScale = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (percentScaled < 1f) 
+        {
+            percentScaled += Time.deltaTime / timeToReachTarget; 
+            float scale = Mathf.Lerp(startScale, targetScale, percentScaled); 
+            transform.localScale = new Vector3(scale, scale, scale); 
+        }
     }
 }
